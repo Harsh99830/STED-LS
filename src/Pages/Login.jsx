dfeb16e05b04083563bf666a75b63847af907bfb
harsh agrawal <harshagrawal7878@gmail.com>
2025-05-22 18:01:04 +0530

    dashboard route

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,7 +5,7 @@ function Login() {
   return (
     <div className="flex items-center justify-center bg-white">
       <div className="scale-130 mt-40"> 
-        <SignIn afterSignInUrl="/Dashboard"/>
+        <SignIn afterSignInUrl="/dashboard"/>
       </div>
     </div>
   );
